b5525ee1fa8fcf941500533d9f2e0619d7994685
MorZLE <art04.04@mail.ru>
2023-03-29 19:24:55 +0300

    5

--- main.py
+++ main.py
@@ -4,64 +4,102 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from prettytable import from_db_cursor
-
+from threading import Thread
+import time
+import requests
 
 
 def main():
     with open('key.txt', encoding='utf-8') as f:
         key_i = f.readline().rstrip()
         key_tg = f.readline().rstrip()
-        admin_id= f.readline().rstrip()
+        admin_id = int(f.readline().rstrip())
 
     openai.api_key = key_i
     bot = Bot(token=key_tg)
     dp = Dispatcher(bot)
+    db = DB()
+    db.con_db_app()
 
     async def start_bot(_):
         global con, cur
         print('Бот онлайн!')
-        con = sl.connect('data_user.db')
+        con = sl.connect('chyrik.db')
         cur = con.cursor()
-        con.execute('CREATE TABLE IF NOT EXISTS USER (ID text, data text,time text);')
         con.commit()
 
     @dp.message_handler(commands=['start'])
     async def com_start(message: types.Message):
-        await message.reply('Привет! Я бот сделанный на основе ИИ. Спроси у меня что хочешь и я постараюсь ответить.')
+        await message.reply('Привет!')
 
-    @dp.message_handler(commands=['HS'])
+    @dp.message_handler(commands=['hs'])
     async def history(message: types.Message):
         if message.from_user.id == int(admin_id):
             await message.reply(from_db_cursor(cur.execute('SELECT * FROM USER;')))
 
-    @dp.message_handler(commands=['clear_hs'])
-    async def history(message: types.Message):
-        if message.from_user.id == admin_id:
-            cur.execute('DELETE FROM USER;')
+    @dp.message_handler(commands=['cl'])
+    async def clear_hh(message: types.Message):
+        if message.from_user.id == int(admin_id):
+            res, _ = db.exec('DELETE FROM USER;')
             await message.reply('Грехи прощены!')
+
+    @dp.message_handler(commands=['ban'])
+    async def ban(message: types.Message):
+        if message.from_user.id == int(admin_id):
+            name = message.text.split()[-1]
+            db.exec('INSERT INTO BAN (name,time) values(?,?)', name, message.date)
+            await message.reply(f'{name} забанен!')
+
+    @dp.message_handler(commands=['unban'])
+    async def unban(message: types.Message):
+        if message.from_user.id == int(admin_id):
+            user = message.text.split()[-1]
+            db.exec(f'DELETE FROM BAN WHERE name=?', user)
+            await message.reply(f'{user} разбанен!')
+
+    @dp.message_handler(commands=['unbanall'])
+    async def unban(message: types.Message):
+        if message.from_user.id == int(admin_id):
+            db.exec(f'DELETE FROM BAN')
+            await message.reply('Все разбанены!')
+
+    @dp.message_handler(commands=['user'])
+    async def unban(message: types.Message):
+        if message.from_user.id == int(admin_id):
+            await message.reply(from_db_cursor(cur.execute('SELECT * FROM BAN;')))
 
     @dp.message_handler()
     async def send(message: types.Message):
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=message.text,
-            temperature=0.9,
-            max_tokens=1000,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.6,
-            stop=["You:"]
-        )
-        con.execute('INSERT INTO USER (ID, data,time) values(?,?,?)',
-                    (message.from_user.username, message.text, message.date))
-        con.commit()
-        await message.reply(response['choices'][0]['text'])
+        name = message.from_user.username
+        res = check_ban(name)
+        if res:
+            await message.reply('Дай-ка подумаю...')
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=message.text,
+                temperature=0.9,
+                max_tokens=1000,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.6,
+                stop=["You:"]
+            )
+            con.execute('INSERT INTO USER (ID, data,time) values(?,?,?)',
+                        (message.from_user.username, message.text, message.date))
+            con.commit()
+            await message.reply(response['choices'][0]['text'])
+        else:
+            await message.reply('вас забанили')
+
+    def check_ban(name):
+        try:
+            res, _ = db.exec('select * from BAN where name=?', name)
+        except sl.OperationalError:
+            return True
+        return len(res) == 0
 
     executor.start_polling(dp, skip_updates=True, on_startup=start_bot)
-
-    pass
 
 
 if __name__ == '__main__':
     main()
-
